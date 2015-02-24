557b748b8e9d86e5325fca8cfa63322cc02b4162
aneesh-neelam <neelam.aneesh@gmail.com>
2015-02-24 23:18:00 +0530

    Misc Changes
    * MongoDB Middleware for Express, Connection Pooling
    * Copyright Notice for all files
    * Reformatted All Code
    * Added New Relic Support
    * Added Logentries Support
    * Added Heroku Procfile
    * Refactored Generated Code

--- app.js
+++ app.js
@@ -1,28 +1,107 @@
+/*
+ *  VITauth
+ *  Copyright (C) 2015  Kishore Narendran <kishore.narendran09@gmail.com>
+ *  Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *  Copyright (C) 2015  Aarthy Kolachalam Chandrasekhar <kcaarthy@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var mongodb = require('express-mongo-db');
+var path = require('path');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var vitapi = require('./routes/vitapi');
-var clientapi = require('./routes/clientapi');
+var newrelic;
+if (process.env.NEWRELIC_APP_NAME && process.env.NEWRELIC_LICENSE) {
+    newrelic = require('newrelic');
+}
+
+var log;
+if (process.env.LOGENTRIES_TOKEN) {
+    var logentries = require('node-logentries');
+    log = logentries.logger({
+        token: process.env.LOGENTRIES_TOKEN
+    });
+}
+
+var routes = require(path.join(__dirname, 'routes', 'index'));
+var users = require(path.join(__dirname, 'routes', 'users'));
+var clientapi = require(path.join(__dirname, 'routes', 'clientapi'));
+var vitapi = require(path.join(__dirname, 'routes', 'vitapi'));
 
 var app = express();
+
+app.set('title', 'VITauth');
+
+var loggerLevel = process.env.LOGGER_LEVEL || 'dev';
+app.use(logger(loggerLevel));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// New Relic in Template
+if (newrelic) {
+    app.locals.newrelic = newrelic;
+}
+
+// Static
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Favicon
+// Uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Cookie Parser
+var secret = process.env.SECRET_KEY || 'randomsecretstring';
+app.use(cookieParser(secret, {signed: true}));
+
+// MongoDB
+var mongodbOptions = {
+    hosts: [{
+        host: process.env.MONGODB_HOST || '127.0.0.1',
+        port: process.env.MONGODB_PORT || '27017'
+    }],
+    database: process.env.MONGODB_DATABASE || 'VITauth',
+    username: process.env.MONGODB_USERNAME,
+    password: process.env.MONGODB_PASSWORD,
+    options: {
+        db: {
+            native_parser: true,
+            recordQueryStats: true,
+            retryMiliSeconds: 500,
+            numberOfRetries: 10
+        },
+        server: {
+            socketOptions: {
+                keepAlive: 1,
+                connectTimeoutMS: 10000
+            },
+            auto_reconnect: true,
+            poolSize: 50
+        }
+    }
+};
+app.use(mongodb(require('mongodb'), mongodbOptions));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -30,35 +109,37 @@ app.use('/vitapi', vitapi);
 app.use('/clientapi', clientapi);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// Error handlers
+// Development error handler, will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
+        if (log) {
+            log.log('debug', {Error: err, Message: err.message});
+        }
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            err: err
         });
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+// Production error handler, no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    if (log) {
+        log.log('debug', {Error: err, Message: err.message});
+    }
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        err: {}
     });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
+/*
+ *  VITauth
+ *  Copyright (C) 2015  Kishore Narendran <kishore.narendran09@gmail.com>
+ *  Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *  Copyright (C) 2015  Aarthy Kolachalam Chandrasekhar <kcaarthy@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'VITauth-Backend System' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'VITauth-Backend System'});
 });
 
 module.exports = router;

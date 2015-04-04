a89b670f99fba9b6fd0ae3c1cca4d544fb0e9d98
aneesh-neelam <neelam.aneesh@gmail.com>
2015-04-04 14:49:29 +0530

    Fixed New Relic Configuration
    * Added newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,34 @@
+/*
+ *  VITauth
+ *  Copyright (C) 2015  Kishore Narendran <kishore.narendran09@gmail.com>
+ *  Copyright (C) 2015  Aneesh Neelam <neelam.aneesh@gmail.com>
+ *  Copyright (C) 2015  Aarthy Kolachalam Chandrasekhar <kcaarthy@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+
+if (process.env.NEWRELIC_APP_NAME && process.env.NEWRELIC_LICENSE) {
+  var app_name = process.env.NEWRELIC_APP_NAME;
+  var license = process.env.NEWRELIC_LICENSE;
+}
+
+exports.config = {
+  app_name: app_name,
+  license_key: license,
+  logging: {
+    level: 'info'
+  }
+};
